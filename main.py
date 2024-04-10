d7e3b78efab28548c970f7a5cca29b56bb28bd09
Leonardo Mopaca <leonardomopaca@gmail.com>
2024-04-10 15:54:11 -0500

    ini

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import load_elnino_periods
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,5 +14,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    load_elnino_periods
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
